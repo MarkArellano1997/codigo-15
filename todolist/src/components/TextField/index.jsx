a869718b97ea2a3799075d49e35bd0d1eaba6a8a
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2023-10-27 22:36:11 -0500

    subiendo todo a git

--- todolist/src/components/TextField/index.jsx
+++ todolist/src/components/TextField/index.jsx
@@ -13,6 +13,7 @@ export default function TextField({
             name={name}
             type={type}
             value={value}
+            aria-label={name}
             onChange={onChange}
             className={`border w-full px-2 py-3 rounded-l outline-none ${className}`}
             placeholder={placeholder}
